f4d31e4e8a8fa5c3ec288101aa414ba502536da6
toady5 <116747600+toady5@users.noreply.github.com>
2023-05-02 20:03:55 +0000

    Co-authored-by: kylesmart2 <kylesmart2@users.noreply.github.com>

--- ITS240_Final_KodySmart.c
+++ ITS240_Final_KodySmart.c
@@ -1,7 +1,24 @@
+/*Kody Smart
+ITS24000 Spring 2023
+Final Project
+5/2/23
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+struct Order
+{
+    double totalCost;
+    double discount;
+    double subtotal;
+    double tax;
+    double total;
+};
+
+void printReceipt();
 
 int main()
 {
@@ -9,4 +26,11 @@ int main()
     return 0;
 }
 
-
+void printReceipt(struct Order receipt)
+{
+    printf("Total item costs: $%8.2f\n", receipt.totalCost);
+    printf("Discount:         $%8.2f\n", receipt.discount);
+    printf("Tax:              $%8.2f\n", receipt.tax);
+    printf("Subtotal:         $%8.2f\n", receipt.subtotal);
+    printf("Total:            $%8.2f\n", receipt.total);
+}//end of printReceipt()
